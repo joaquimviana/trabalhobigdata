5260343d039f0a532933a345682620a711fd0a0f
joaquim <joaquim@joaquim-270E5K-270E5Q-271E5K-2570EK>
2016-12-19 17:08:29 -0200

    Versão final

--- scripts/load_data_chart.py
+++ scripts/load_data_chart.py
@@ -1,4 +1,4 @@
-
+## Imports Necessários ao Cpython ##
 from __future__ import print_function
 import pandas as pd
 import sys
@@ -6,54 +6,49 @@ import os
 SPARK_HOME = os.environ["SPARK_HOME"]="/srv/spark"
 PYTHONPATH=os.environ["PYTHONPATH"]= "/srv/spark/python/lib/py4j-0.8.2.1-src.zip:/srv/spark/spark/python"
 
-
+## Adicionando o PYSPARK ao path ##
 sys.path.append("/srv/spark/python/pyspark") 
 sys.path.append("/srv/spark/python") 
 sys.path.append("/srv/spark/python/lib/py4j") 
 sys.path.append("/srv/spark/python/lib") 
 sys.path.append("/srv/spark/python/pyspark/accumulators") 
-from operator import add
+
+## Import do Pyspark ##
 from pyspark.sql import SparkSession
 
 
-param = config.loc[0,"param"]
+## Recebe os parametros dos Filtros ##
 mesParam = config.loc[0,"mesParam"]
 anoParam = config.loc[0,"anoParam"]
 dominioParam = config.loc[0,"dominioParam"]
 
-
-
-
-
-
-
+## Inicia a Sessao no master Local ##
 spark = SparkSession\
     .builder\
     .appName("PythonQuery")\
     .getOrCreate()
 
+##Carrega ods Dados para o RDD
 lines = spark.read.csv('/home/joaquim/TrabalhoSpark/dados/PageViewsPerDayAll.csv')
-
+## Organiza os dados
 lines = lines.rdd.map(lambda row: [row[0],row[1].split("/")[0], row[1].split("/")[1], row[1].split("/")[2],row[2]])
 
-if(dominioParam != "Todos"):
-	lines = lines.filter(lambda w: w[0].startswith(dominioParam))
-#if(anoParam != 0):
-#	lines = lines.filter(lambda w: w[0].startswith(dominioParam))
-#if(dominioParam != 0):
-#	lines = lines.filter(lambda w: w[0].startswith(dominioParam))
+## Faz a Seleção dos dados segundo os filtros
+lines = lines.filter(lambda w: w[0] == dominioParam)
+lines = lines.filter(lambda w: int(w[1]) == anoParam)
+lines = lines.filter(lambda w: int(w[2]) == mesParam)
 
+## Map (Dia / Qtd)
+lines = lines.map(lambda x: (int(x[3]), int(x[4])))
+##Agrega Qtd total / qtd
+lines = lines.aggregateByKey((0,0), lambda a,b: (a[0] + b,    a[1] + 1), lambda a,b: (a[0] + b[0], a[1] + b[1]))
 
-
-counts = lines.map(lambda x: (int(x[1]), 1)).reduceByKey(add)
-
+counts = lines.mapValues(lambda v: v[0]/v[1])
+## Ordena dos dados pelo dia
 counts = counts.sortBy(lambda a: a[0])
-
+## Coleta os resultados
 output = counts.collect()
 
-for (day, count) in output:
-    print("%s: %i" % (day, count))
-
-
+## Salva o resultado em um Padas data frame
 res = pd.DataFrame(output)
 
--- scripts/load_data_filter_dominio.py
+++ scripts/load_data_filter_dominio.py
@@ -1,35 +1,32 @@
+## Imports Necessários ao Cpython ##
 from __future__ import print_function
 import pandas as pd
+from operator import add
 import sys
 import os
+
+## Adicionando o PYSPARK ao path ##
 SPARK_HOME = os.environ["SPARK_HOME"]="/srv/spark"
 PYTHONPATH=os.environ["PYTHONPATH"]= "/srv/spark/python/lib/py4j-0.8.2.1-src.zip:/srv/spark/spark/python"
-
-
 sys.path.append("/srv/spark/python/pyspark") 
 sys.path.append("/srv/spark/python") 
 sys.path.append("/srv/spark/python/lib/py4j") 
 sys.path.append("/srv/spark/python/lib") 
 sys.path.append("/srv/spark/python/pyspark/accumulators") 
-from operator import add
+
+## Import do Pyspark ##
 from pyspark.sql import SparkSession
 
 
-param = config.loc[0,"param"]
-mesParam = config.loc[0,"mesParam"]
-anoParam = config.loc[0,"anoParam"]
-dominioParam = config.loc[0,"dominioParam"]
-
-
-
-
+## Inicia a Sessao no master Local ##
 spark = SparkSession\
     .builder\
     .appName("PythonQuery")\
     .getOrCreate()
 
+##Carrega ods Dados
 lines = spark.read.csv('/home/joaquim/TrabalhoSpark/dados/PageViewsPerDayAll.csv')
-
+## Organiza os dados
 lines = lines.rdd.map(lambda row: [row[0],row[1].split("/")[0], row[1].split("/")[1], row[1].split("/")[2],row[2]])
 
 counts = lines.map(lambda x: (x[0], -int(x[4]))).reduceByKey(add)
@@ -37,10 +34,6 @@ counts = lines.map(lambda x: (x[0], -int(x[4]))).reduceByKey(add)
 counts = counts.sortBy((lambda a: a[1]))
 
 output = counts.collect()
-
-for (day, count) in output:
-    print("%s: %i" % (day, count))
-
 
 res = pd.DataFrame(output)
 
